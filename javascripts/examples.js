041476459540b01167a31757598c7d599796dc28
Jason Mulligan <jason.mulligan@avoidwork.com>
2014-02-01 23:59:06 -0500

    Updating keigai & specifying a data list sort

--- javascripts/examples.js
+++ javascripts/examples.js
@@ -2,7 +2,7 @@
 	var fields  = ["name", "age"],
 	    options = {pageSize: 5, order: "age desc, name"},
 	    store   = keigai.store(),
-        list    = keigai.list( document.querySelector( "#create-list" ), store, "{{name}}" ),
+        list    = keigai.list( document.querySelector( "#create-list" ), store, "{{name}}", {order: "name"} ),
         grid    = keigai.grid( document.querySelector( "#create-grid" ), store, fields, fields, options, true);
 
 	store.setUri( "data.json" );
