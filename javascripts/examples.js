f791a273e82e1e372610420b7927db2f02d383a3
Jason Mulligan <jason.mulligan@avoidwork.com>
2014-02-01 23:35:01 -0500

    Adding a lot of content

---
+++ javascripts/examples.js
@@ -0,0 +1,9 @@
+( function ( document ) {
+	var fields  = ["name", "age"],
+	    options = {pageSize: 5, order: "age desc, name"},
+	    store   = keigai.store(),
+        list    = keigai.list( document.querySelector( "#create-list" ), store, "{{name}}" ),
+        grid    = keigai.grid( document.querySelector( "#create-grid" ), store, fields, fields, options, true);
+
+	store.setUri( "data.json" );
+})( document );
