ed66f3863fa4b9bd6dd5739b63018b245c2c43fa
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-04-22 07:35:30 -0400

    Minor change to `store.sort()` such that it does a little less work in some cases

--- src/store.js
+++ src/store.js
@@ -690,9 +690,9 @@ class DataStore extends Base {
 				}
 			} else {
 				clauses = array.fromObject( where );
-				sorted = array.flat( clauses.map( function ( i ) { return i.toString(); } ) ).filter( ( i, idx ) => {
+				sorted = array.flat( clauses ).filter( ( i, idx ) => {
 					return idx % 2 === 0;
-				} ).sort( array.sort );
+				} ).map( function ( i ) { return i.toString(); } ).sort( array.sort );
 				index = sorted.join( "|" );
 				values = sorted.map( ( i ) => {
 					return where[ i ];
